441632df74ec9fa026e49a9a98696f4e5f6bba07
Yadvendra Sharma <yadvendras20@gmail.com>
2024-10-21 14:25:47 +0530

    m

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 const app = express();
 const httpServer = createServer(app);
 
-const redisClient = new Redis("rediss://default:AeQcAAIjcDE0MjMyYTMzNDEwYzc0Y2ZiOWFkMzk1M2JlZTgwM2IwMHAxMA@helpful-polliwog-58396.upstash.io:6379");
+const redisClient = new Redis("redis://127.0.0.1:6379");
 app.use(express.json());
 app.use(cookieParser());
 
